0ce8570209be1a3f43204ea5e843e06409cc48e0
Makaratel <pmakaewskij@mail.ru>
2019-06-30 09:07:08 +0300

    Get data from server

--- js/data.js
+++ js/data.js
@@ -1,49 +1,30 @@
 'use strict';
 
 (function () {
-  var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-  var PIN_WIDTH = 65;
-
   var map = document.querySelector('.map');
   var pinList = map.querySelector('.map__pins');
   var pin = document.querySelector('#pin').content.querySelector('.map__pin');
-  var offers = [];
 
-  var getRandomOffers = function () {
-    for (var i = 0; i < 8; i++) {
-      var authorAvatar = 'img/avatars/user0' + (i + 1) + '.png';
-      var offerType = OFFER_TYPE[window.util.getRandomNumber(0, OFFER_TYPE.length)];
-      var offerCoordinateX = window.util.getRandomNumber(PIN_WIDTH, 1200 - PIN_WIDTH) + 'px';
-      var offerCoordinateY = window.util.getRandomNumber(130, 630) + 'px';
-      var offerCoordinates = [offerCoordinateX, offerCoordinateY];
-
-      var offer = {
-        author: authorAvatar,
-        offer: offerType,
-        location: offerCoordinates
-      };
-      offers.push(offer);
-    }
-    return offers;
-  };
-
-  var renderOffers = function () {
+  var renderOffers = function (offers) {
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < offers.length; i++) {
       var offerElement = pin.cloneNode(true);
-      offerElement.style.left = offers[i].location[0];
-      offerElement.style.top = offers[i].location[1];
-      offerElement.children[0].src = offers[i].author;
-      offerElement.children[0].alt = 'Заголовок объявления';
+      offerElement.style.left = offers[i].location.x + 'px';
+      offerElement.style.top = offers[i].location.y + 'px';
+      offerElement.children[0].src = offers[i].author.avatar;
+      offerElement.children[0].alt = offers[i].offer.title;
 
       fragment.appendChild(offerElement);
     }
     pinList.appendChild(fragment);
   };
 
+  var getData = function () {
+    window.backend.load(renderOffers);
+  }
+
   window.data = {
-    getRandomOffers: getRandomOffers,
-    renderOffers: renderOffers
+    getData: getData
   };
 })();
